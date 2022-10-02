09da36e53a35e5690c2a6b6c164a0d1a582481b7
stefba <st@rg-s.com>
2022-10-02 18:14:11 +0200

    First mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 function App() {
+  const [value, setValue] = React.useState<Dayjs | null>(
+    dayjs('2014-08-18T21:11:54'),
+  );
+
+  const handleChange = (newValue: Dayjs | null) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Kine</h1>
+      <div>
+        Title: <input type="text" />
+      </div>
+      <div>
+      Title-En: <input type="text" />
+      </div>
+      <br />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/DD/YYYY"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <div>
+        Transcript De: <textarea></textarea>
+      </div>
+      <div>
+        Transcript En: <textarea></textarea>
+      </div>
+      <div>
+        Cover:
+        <input type="file" />
+      </div>
+      <div>
+        Subtitles De:
+        <input type="file" />
+      </div>
+      <div>
+        Subtitles En:
+        <input type="file" />
+      </div>
+    </main>
   );
 }
 
